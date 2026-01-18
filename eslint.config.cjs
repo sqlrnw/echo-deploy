7f21e10ff021aed1f411e1f0b9057c17fb3fe4d4
Mohamed Ragap <medoragap7@gmail.com>
2026-01-18 11:30:43 +0200

    fix bug

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,17 +1,35 @@
 const js = require("@eslint/js");
 const jest = require("eslint-plugin-jest");
+const globals = require("globals");
 
 module.exports = [
+  // Base JS + Node
   {
     files: ["**/*.js"],
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: "commonjs"
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node
+      }
     },
     plugins: { jest },
     rules: {
-      ...js.configs.recommended.rules,
+      ...js.configs.recommended.rules
+    }
+  },
+
+  // Jest tests
+  {
+    files: ["test/**/*.js", "**/*.test.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    },
+    rules: {
       ...jest.configs.recommended.rules
     }
   }
 ];
+
